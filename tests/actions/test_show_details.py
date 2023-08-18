ae08eefd1b25dc628a6fafabaf03c5a2e20f8715
Pietro Bucker <pietro_177@hotmail.com>
2023-08-18 19:26:08 -0300

    arrumando requisto 2 pois deu erro no avaliodar online

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -40,3 +40,36 @@ def test_show_details_expect_response(context, expect, capsys):
 def test_show_details_expect_fail(context2, expect):
     with pytest.raises(expect):
         show_details(context2)
+
+
+def test_show_details_expect_expected_tmp_path(monkeypatch, tmp_path, capsys):
+    new_path = tmp_path / "teste.txt"
+    new_path2 = tmp_path / "teste2.txt"
+    new_path.write_text("heloo")
+    new_path2.write_text("hello word!")
+    new_path = str(new_path)
+    new_path2 = str(new_path2)
+    
+    context = {"base_path": new_path}
+    show_details(context)
+    capt = capsys.readouterr()
+    assert (
+        capt.out
+        == """File name: teste.txt
+File size in bytes: 5
+File type: file
+File extension: .txt
+Last modified date: 2023-08-18\n"""
+    )
+    
+    context = {"base_path": new_path2}
+    show_details(context)
+    capt = capsys.readouterr()
+    assert (
+        capt.out
+        == """File name: teste2.txt
+File size in bytes: 11
+File type: file
+File extension: .txt
+Last modified date: 2023-08-18\n"""
+    )
